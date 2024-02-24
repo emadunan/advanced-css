956f507702d6005e186073706a0c807739678729
Emad Younan <emadunan@gmail.com>
2024-02-24 23:02:24 +0200

    chore: Create the-wild-oasis project

---
+++ the-wild-oasis/src/App.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>Hello world</H1>
+        <Button onClick={() => alert("Hi")}>Check 1</Button>
+        <Button onClick={() => alert("Hi")}>Check 2</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
